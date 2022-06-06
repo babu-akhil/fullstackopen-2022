ae92aa677ed7da57337d7a3982853ea094bcbb81
akhil <47088680+babu-akhil@users.noreply.github.com>
2022-06-06 15:02:09 +0530

    Add files via upload

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,80 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p><strong>total of {sum} exercises</strong></p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+
+  <>
+    {parts.map(part => <Part part = {part} key = {part.id}/>)}  
+  </>
+
+const Course = ({course}) => {
+  console.log(course)
+  return (
+    <div>
+      <Header course = {course.name}/>
+      <Content parts = {course.parts} />
+      <Total sum = {course.parts.reduce((a, b)=>(a + b.exercises),0)}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      {courses.map(course => <Course course = {course} key={course.id}/>)}
+    </div>
+  )
+}
+
+export default App
